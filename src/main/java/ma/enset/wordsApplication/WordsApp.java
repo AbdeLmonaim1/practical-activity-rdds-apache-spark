d3f0b0e57f12bc7f828313b876a3676352cf0735
AbdeLmonaim1 <abdelmonaimahdoud3@gmail.com>
2025-12-25 23:46:00 +0100

    deploy the application in s spark cluster, and the file processed was store in the hfds

--- src/main/java/ma/enset/wordsApplication/WordsApp.java
+++ src/main/java/ma/enset/wordsApplication/WordsApp.java
@@ -12,13 +12,22 @@ public class WordsApp {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf()
                 .setAppName("Words Application")
-                .setMaster("local[*]");
+                //We commented this line be cause now we deploy our app in docker container
+//                .setMaster("local[*]")
+                ;
         JavaSparkContext sc = new JavaSparkContext(conf);
         //Each line in the file is an element in the RDD
-        JavaRDD<String> readLines = sc.textFile("words.txt");
+//        JavaRDD<String> readLines = sc.textFile("words.txt");
+        //now if the words.txt in the hdfs in hadoop cluster
+        JavaRDD<String> readLines = sc.textFile("hdfs://namenode:8020/words.txt");
         JavaRDD<String> wordsSplitedRDD = readLines.flatMap(line -> Arrays.asList(line.split(" ")).iterator());
         JavaPairRDD<String, Integer> pairWordsRDD = wordsSplitedRDD.mapToPair(word -> new Tuple2<>(word, 1));
         JavaPairRDD<String, Integer> numberOccurenceForEachWord = pairWordsRDD.reduceByKey((v1, v2) -> v1 + v2);
         numberOccurenceForEachWord.collect().forEach(System.out::println);
+        //We put right now the words.txt in HDFS in hadoop container by using thoses commands docker cp .\words.txt practical-activity-rdds-namenode-1:/opt/hadoop, and hdfs dfs -put words.txt / inside of container
+        //Now we want to deployed application in spark cluster which executed in Docker container
+        //So we need to generate a .jar file of the application an put it in the spark master container
+        //by using this command docker cp .\practical-activity-rdds-1.0-SNAPSHOT.jar spark-master:/opt/spark/work-dir
+        // /opt/spark/bin/spark-submit --master spark://spark-master:7077 --class ma.enset.wordsApplication practical-activity-rdds-1.0-SNAPSHOT.jar : for deploying the app in spark cluster
     }
 }
